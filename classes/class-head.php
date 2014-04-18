9450148491454d13eca606091917780c93597671
Barry Kooij <git@barrykooij.nl>
2014-04-18 10:25:00 +0200

    Added an action in the construct.
    Because we're adding the hooks in an OOP way it's nearly impossible for third-party applications to remove them. I've solved this issue by adding filters. The WPSEO News head tags can now be disabled with the following filters.
    
    add_filter('wpseo_news_head_display_keywords', '__return_false');
    add_filter('wpseo_news_head_display_original', '__return_false');
    add_filter('wpseo_news_head_display_standout', '__return_false');
    
    Fixes #58.

--- classes/class-head.php
+++ classes/class-head.php
@@ -3,6 +3,13 @@
 class WPSEO_News_Head {
 
 	/**
+	 * WPSEO_News_Head Constructor
+	 */
+	public function __construct() {
+		do_action( 'wpseo_news_head' );
+	}
+
+	/**
 	 * Display the optional sources link elements in the <code>&lt;head&gt;</code>.
 	 */
 	public function add_head_tags() {
@@ -10,26 +17,33 @@ class WPSEO_News_Head {
 		if ( is_singular() ) {
 			global $post;
 
-			$meta_news_keywords = trim( WPSEO_Meta::get_value( 'newssitemap-keywords', $post->ID ) );
-			if ( ! empty( $meta_news_keywords ) ) {
-				echo '<meta name="news_keywords" content="' . $meta_news_keywords . '" />' . "\n";
+			if ( apply_filters( 'wpseo_news_head_display_keywords', true ) ) {
+				$meta_news_keywords = trim( WPSEO_Meta::get_value( 'newssitemap-keywords', $post->ID ) );
+				if ( ! empty( $meta_news_keywords ) ) {
+					echo '<meta name="news_keywords" content="' . $meta_news_keywords . '" />' . "\n";
+				}
 			}
 
-			$original_source = trim( WPSEO_Meta::get_value( 'newssitemap-original', $post->ID ) );
-			if ( empty( $original_source ) ) {
-				echo '<link rel="original-source" href="' . get_permalink( $post->ID ) . '" />' . "\n";
-			} else {
-				$sources = explode( '|', $original_source );
-				foreach ( $sources as $source ) {
-					echo '<link rel="original-source" href="' . $source . '" />' . "\n";
+			if ( apply_filters( 'wpseo_news_head_display_original', true ) ) {
+				$original_source = trim( WPSEO_Meta::get_value( 'newssitemap-original', $post->ID ) );
+				if ( empty( $original_source ) ) {
+					echo '<link rel="original-source" href="' . get_permalink( $post->ID ) . '" />' . "\n";
+				} else {
+					$sources = explode( '|', $original_source );
+					foreach ( $sources as $source ) {
+						echo '<link rel="original-source" href="' . $source . '" />' . "\n";
+					}
 				}
 			}
 
 			// Standout tag
-			$meta_standout = WPSEO_Meta::get_value( 'newssitemap-standout', $post->ID );
-			if ( 'on' == $meta_standout ) {
-				echo '<meta name="standout" content="' . get_permalink( $post->ID ) . '"/>' . "\n";
+			if ( apply_filters( 'wpseo_news_head_display_standout', true ) ) {
+				$meta_standout = WPSEO_Meta::get_value( 'newssitemap-standout', $post->ID );
+				if ( 'on' == $meta_standout ) {
+					echo '<meta name="standout" content="' . get_permalink( $post->ID ) . '"/>' . "\n";
+				}
 			}
+
 		}
 
 	}
