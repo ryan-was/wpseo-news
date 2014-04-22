f199bf11ae5693779ee3662792cea0306776904a
Barry Kooij <git@barrykooij.nl>
2014-04-22 10:20:43 +0200

    Added $post to filters and updated documentation.

--- classes/class-head.php
+++ classes/class-head.php
@@ -21,9 +21,9 @@ class WPSEO_News_Head {
 			 * Filter: 'wpseo_news_head_display_keywords' - Allow preventing of outputting news keywords tag
 			 *
 			 * @api string $meta_news_keywords The meta news keywords tag
-			 *
+			 * @param object $post The post
 			 */
-			if ( apply_filters( 'wpseo_news_head_display_keywords', true ) ) {
+			if ( apply_filters( 'wpseo_news_head_display_keywords', true, $post ) ) {
 				$meta_news_keywords = trim( WPSEO_Meta::get_value( 'newssitemap-keywords', $post->ID ) );
 				if ( ! empty( $meta_news_keywords ) ) {
 					echo '<meta name="news_keywords" content="' . $meta_news_keywords . '" />' . "\n";
@@ -31,12 +31,12 @@ class WPSEO_News_Head {
 			}
 
 			/**
-			 * Filter: 'wpseo_news_head_display_keywords' - Allow preventing of outputting news keywords tag
+			 * Filter: 'wpseo_news_head_display_keywords' - Allow preventing of outputting original source tag
 			 *
 			 * @api string $meta_news_keywords The meta news keywords tag
-			 *
+			 * @param object $post The post
 			 */
-			if ( apply_filters( 'wpseo_news_head_display_original', true ) ) {
+			if ( apply_filters( 'wpseo_news_head_display_original', true, $post ) ) {
 				$original_source = trim( WPSEO_Meta::get_value( 'newssitemap-original', $post->ID ) );
 				if ( empty( $original_source ) ) {
 					echo '<link rel="original-source" href="' . get_permalink( $post->ID ) . '" />' . "\n";
@@ -52,9 +52,9 @@ class WPSEO_News_Head {
 			 * Filter: 'wpseo_news_head_display_standout' - Allow preventing of outputting standout tag
 			 *
 			 * @api string $meta_standout The standout tag
-			 *
+			 * @param object $post The post
 			 */
-			if ( apply_filters( 'wpseo_news_head_display_standout', true ) ) {
+			if ( apply_filters( 'wpseo_news_head_display_standout', true, $post ) ) {
 				$meta_standout = WPSEO_Meta::get_value( 'newssitemap-standout', $post->ID );
 				if ( 'on' == $meta_standout ) {
 					echo '<meta name="standout" content="' . get_permalink( $post->ID ) . '"/>' . "\n";
