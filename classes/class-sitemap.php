7f7b8bec3988dbedafaefe95fc6e637a597008d9
Barry Kooij <git@barrykooij.nl>
2014-06-05 14:28:30 +0200

    Fixed publication_date timezone bug

--- classes/class-sitemap.php
+++ classes/class-sitemap.php
@@ -64,6 +64,46 @@ class WPSEO_News_Sitemap {
 	}
 
 	/**
+	 * Returns the timezone string for a site, even if it's set to a UTC offset
+	 *
+	 * Adapted from http://www.php.net/manual/en/function.timezone-name-from-abbr.php#89155
+	 *
+	 * @return string valid PHP timezone string
+	 */
+	private function wp_get_timezone_string() {
+
+		// if site timezone string exists, return it
+		if ( $timezone = get_option( 'timezone_string' ) )
+			return $timezone;
+
+		// get UTC offset, if it isn't set then return UTC
+		if ( 0 === ( $utc_offset = get_option( 'gmt_offset', 0 ) ) )
+			return 'UTC';
+
+		// adjust UTC offset from hours to seconds
+		$utc_offset *= 3600;
+
+		// attempt to guess the timezone string from the UTC offset
+		$timezone = timezone_name_from_abbr( '', $utc_offset );
+
+		// last try, guess timezone string manually
+		if ( false === $timezone ) {
+
+			$is_dst = date( 'I' );
+
+			foreach ( timezone_abbreviations_list() as $abbr ) {
+				foreach ( $abbr as $city ) {
+					if ( $city['dst'] == $is_dst && $city['offset'] == $utc_offset )
+						return $city['timezone_id'];
+				}
+			}
+		}
+
+		// fallback to UTC
+		return 'UTC';
+	}
+
+	/**
 	 * Build the sitemap and push it to the XML Sitemaps Class instance for display.
 	 */
 	public function build() {
@@ -97,6 +137,7 @@ class WPSEO_News_Sitemap {
 		xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">' . "\n";
 
 //		echo '<!--' . print_r( $items, 1 ) . '-->';
+
 
 		if ( ! empty( $items ) ) {
 			foreach ( $items as $item ) {
@@ -171,7 +212,10 @@ class WPSEO_News_Sitemap {
 					$output .= "\t\t<news:genres>" . htmlspecialchars( $genre ) . '</news:genres>' . "\n";
 				}
 
-				$output .= "\t\t<news:publication_date>" . mysql2date( 'c', $item->post_date_gmt ) . '</news:publication_date>' . "\n";
+				// Create a DateTime object date in the correct timezone
+				$datetime = new DateTime( $item->post_date_gmt, new DateTimeZone( $this->wp_get_timezone_string() ) );
+
+				$output .= "\t\t<news:publication_date>" . $datetime->format( 'c' ) . '</news:publication_date>' . "\n";
 				$output .= "\t\t<news:title>" . htmlspecialchars( $item->post_title ) . '</news:title>' . "\n";
 
 				if ( ! empty( $keywords ) ) {
