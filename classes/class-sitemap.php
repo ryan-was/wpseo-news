46929e11631b99f339c599e4573c568e4c77ee82
Andy Meerwaldt <andy@yoast.com>
2014-11-06 10:48:03 +0100

    Refactored some code and fixed image source location

--- classes/class-sitemap.php
+++ classes/class-sitemap.php
@@ -8,32 +8,6 @@ class WPSEO_News_Sitemap {
 		$this->options = WPSEO_News::get_options();
 	}
 
-	/**
-	 * Get attachment
-	 *
-	 * @param $attachment_id
-	 *
-	 * @return array
-	 */
-	private function get_attachment( $attachment_id ) {
-		// Get attachment
-		$attachment = get_post( $attachment_id );
-
-		// Check if we've found an attachment
-		if ( null == $attachment ) {
-			return array();
-		}
-
-		// Return props
-		return array(
-			'alt'         => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
-			'caption'     => $attachment->post_excerpt,
-			'description' => $attachment->post_content,
-			'href'        => get_permalink( $attachment->ID ),
-			'src'         => $attachment->guid,
-			'title'       => $attachment->post_title
-		);
-	}
 
 	/**
 	 * Register the XML News sitemap with the main sitemap class.
@@ -51,7 +25,7 @@ class WPSEO_News_Sitemap {
 	 *
 	 * @return string
 	 */
-	function add_to_index( $str ) {
+	public function add_to_index( $str ) {
 
 		$date = new DateTime( get_lastpostdate( 'gmt' ), new DateTimeZone( $this->wp_get_timezone_string() ) );
 
@@ -71,78 +45,12 @@ class WPSEO_News_Sitemap {
 	}
 
 	/**
-	 * Returns the timezone string for a site, even if it's set to a UTC offset
-	 *
-	 * Adapted from http://www.php.net/manual/en/function.timezone-name-from-abbr.php#89155
-	 *
-	 * @return string valid PHP timezone string
-	 */
-	private function wp_get_timezone_string() {
-
-		// if site timezone string exists, return it
-		if ( $timezone = get_option( 'timezone_string' ) ) {
-			return $timezone;
-		}
-
-		// get UTC offset, if it isn't set then return UTC
-		if ( 0 === ( $utc_offset = get_option( 'gmt_offset', 0 ) ) ) {
-			return 'UTC';
-		}
-
-		// adjust UTC offset from hours to seconds
-		$utc_offset *= 3600;
-
-		// attempt to guess the timezone string from the UTC offset
-		$timezone = timezone_name_from_abbr( '', $utc_offset );
-
-		// last try, guess timezone string manually
-		if ( false === $timezone ) {
-
-			$is_dst = date( 'I' );
-
-			foreach ( timezone_abbreviations_list() as $abbr ) {
-				foreach ( $abbr as $city ) {
-					if ( $city['dst'] == $is_dst && $city['offset'] == $utc_offset ) {
-						return $city['timezone_id'];
-					}
-				}
-			}
-		}
-
-		// fallback to UTC
-		return 'UTC';
-	}
-
-	/**
 	 * Build the sitemap and push it to the XML Sitemaps Class instance for display.
 	 */
 	public function build() {
-		global $wpdb;
 
-		$home_url = home_url();
-
-		// Get supported post types
-		$post_types = array();
-		foreach ( get_post_types( array( 'public' => true ), 'objects' ) as $posttype ) {
-			if ( isset( $this->options['newssitemap_include_' . $posttype->name] ) && ( 'on' == $this->options['newssitemap_include_' . $posttype->name] ) ) {
-				$post_types[] = $posttype->name;
-			}
-		}
-
-		if ( count( $post_types ) > 0 ) {
-			$post_types = "'" . implode( "','", $post_types ) . "'";
-		} else {
-			$post_types = "'post'";
-		}
-
-		// Get posts for the last two days only, credit to Alex Moss for this code.
-		$items = $wpdb->get_results( "SELECT ID, post_content, post_name, post_author, post_parent, post_date_gmt, post_date, post_date_gmt, post_title, post_type
-									FROM $wpdb->posts
-									WHERE post_status='publish'
-									AND (DATEDIFF(CURDATE(), post_date_gmt)<=2)
-									AND post_type IN ($post_types)
-									ORDER BY post_date_gmt DESC
-									LIMIT 0, 1000" );
+		$post_types = $this->get_post_types();
+		$items      = $this->get_items( $post_types );
 
 		$output = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">' . "\n";
 
@@ -189,29 +97,8 @@ class WPSEO_News_Sitemap {
 
 				}
 
-				$keywords = explode( ',', trim( WPSEO_Meta::get_value( 'newssitemap-keywords', $item->ID ) ) );
-				$tags     = get_the_terms( $item->ID, 'post_tag' );
-				if ( $tags ) {
-					foreach ( $tags as $tag ) {
-						$keywords[] = $tag->name;
-					}
-				}
-
-				// TODO: add suggested keywords to each post based on category, next to the entire site
-				if ( isset( $this->options['default_keywords'] ) && $this->options['default_keywords'] != '' ) {
-					$keywords = array_merge( $keywords, explode( ',', $this->options['default_keywords'] ) );
-				}
-				$keywords = strtolower( trim( implode( ', ', $keywords ), ', ' ) );
-
-				$genre = WPSEO_Meta::get_value( 'newssitemap-genre', $item->ID );
-				if ( is_array( $genre ) ) {
-					$genre = implode( ',', $genre );
-				}
-
-				if ( $genre == '' && isset( $this->options['default_genre'] ) && $this->options['default_genre'] != '' ) {
-					$genre = $this->options['default_genre'];
-				}
-				$genre = trim( preg_replace( '/^none,?/', '', $genre ) );
+				$keywords = $this->get_item_keywords( $item->ID );
+				$genre    = $this->get_item_genre( $item->ID );
 
 				$stock_tickers = trim( WPSEO_Meta::get_value( 'newssitemap-stocktickers' ) );
 				if ( $stock_tickers != '' ) {
@@ -243,96 +130,9 @@ class WPSEO_News_Sitemap {
 				$output .= $stock_tickers;
 				$output .= "\t</news:news>\n";
 
-				$images = array();
-				if ( ( ! isset( $this->options['restrict_sitemap_featured_img'] ) || ! $this->options['restrict_sitemap_featured_img'] ) && preg_match_all( '/<img [^>]+>/', $item->post_content, $matches ) ) {
-					foreach ( $matches[0] as $img ) {
-						if ( preg_match( '/src=("|\')([^"|\']+)("|\')/', $img, $match ) ) {
-							$src = $match[2];
-							if ( strpos( $src, 'http' ) !== 0 ) {
-
-								if ( $src[0] != '/' ) {
-									continue;
-								}
-
-								$src = $home_url . $src;
-							}
-
-							if ( $src != esc_url( $src ) ) {
-								continue;
-							}
-
-							if ( isset( $url['images'][$src] ) ) {
-								continue;
-							}
-
-							$image = array();
-							if ( preg_match( '/title=("|\')([^"\']+)("|\')/', $img, $match ) ) {
-								$image['title'] = str_replace( array( '-', '_' ), ' ', $match[2] );
-							}
-
-							if ( preg_match( '/alt=("|\')([^"\']+)("|\')/', $img, $match ) ) {
-								$image['alt'] = str_replace( array( '-', '_' ), ' ', $match[2] );
-							}
-
-							$images[$src] = $image;
-						}
-					}
-				}
-
-				// Also check if the featured image value is set.
-				$post_thumbnail_id = get_post_thumbnail_id( $item->ID );
-
-				if ( '' != $post_thumbnail_id ) {
-
-					$attachment = $this->get_attachment( $post_thumbnail_id );
-
-					if ( count( $attachment ) > 0 ) {
-
-						$image = array();
-
-						if ( '' != $attachment['title'] ) {
-							$image['title'] = $attachment['title'];
-						}
-
-						if ( '' != $attachment['alt'] ) {
-							$image['alt'] = $attachment['alt'];
-						}
-
-						if ( '' != $attachment['href'] ) {
-							$images[$attachment['href']] = $image;
-						} else {
-							$images[$attachment['src']] = $image;
-						}
-
-					}
-
-				}
-
-				if ( isset( $images ) && count( $images ) > 0 ) {
-					foreach ( $images as $src => $img ) {
-
-						/**
-						 * Filter: 'wpseo_xml_sitemap_img_src' - Allow changing of sitemap image src
-						 *
-						 * @api string $src The image source
-						 *
-						 * @param object $item The post item
-						 */
-						$src = apply_filters( 'wpseo_xml_sitemap_img_src', $src, $item );
-
-						$output .= "\t\t<image:image>\n";
-						$output .= "\t\t\t<image:loc>" . htmlspecialchars( $src ) . "</image:loc>\n";
-
-						if ( isset( $img['title'] ) ) {
-							$output .= "\t\t\t<image:title>" . htmlspecialchars( $img['title'] ) . "</image:title>\n";
-						}
-
-						if ( isset( $img['alt'] ) ) {
-							$output .= "\t\t\t<image:caption>" . htmlspecialchars( $img['alt'] ) . "</image:caption>\n";
-						}
-
-						$output .= "\t\t</image:image>\n";
-					}
+				// Get images
+				if ( $image_output = $this->get_item_image_output( $item ) ) {
+					$output .= $image_output;
 				}
 
 				$output .= '</url>' . "\n";
@@ -342,6 +142,312 @@ class WPSEO_News_Sitemap {
 		$output .= '</urlset>';
 		$GLOBALS['wpseo_sitemaps']->set_stylesheet( '<?xml-stylesheet type="text/xsl" href="' . preg_replace( '/^http[s]?:/', '', plugin_dir_url( WPSEO_News::get_file() ) ) . 'assets/xml-news-sitemap.xsl"?>' );
 		$GLOBALS['wpseo_sitemaps']->set_sitemap( $output );
+	}
+
+	/**
+	 * Returns the timezone string for a site, even if it's set to a UTC offset
+	 *
+	 * Adapted from http://www.php.net/manual/en/function.timezone-name-from-abbr.php#89155
+	 *
+	 * @return string valid PHP timezone string
+	 */
+	private function wp_get_timezone_string() {
+
+		// if site timezone string exists, return it
+		if ( $timezone = get_option( 'timezone_string' ) ) {
+			return $timezone;
+		}
+
+		// get UTC offset, if it isn't set then return UTC
+		if ( 0 === ( $utc_offset = get_option( 'gmt_offset', 0 ) ) ) {
+			return 'UTC';
+		}
+
+		// adjust UTC offset from hours to seconds
+		$utc_offset *= 3600;
+
+		// attempt to guess the timezone string from the UTC offset
+		$timezone = timezone_name_from_abbr( '', $utc_offset );
+
+		// last try, guess timezone string manually
+		if ( false === $timezone ) {
+
+			$is_dst = date( 'I' );
+
+			foreach ( timezone_abbreviations_list() as $abbr ) {
+				foreach ( $abbr as $city ) {
+					if ( $city['dst'] == $is_dst && $city['offset'] == $utc_offset ) {
+						return $city['timezone_id'];
+					}
+				}
+			}
+		}
+
+		// fallback to UTC
+		return 'UTC';
+	}
+
+	/**
+	 * Get attachment
+	 *
+	 * @param $attachment_id
+	 *
+	 * @return array
+	 */
+	private function get_attachment( $attachment_id ) {
+		// Get attachment
+		$attachment = get_post( $attachment_id );
+
+		// Check if we've found an attachment
+		if ( null == $attachment ) {
+			return array();
+		}
+
+		// Return props
+		return array(
+			'alt'         => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
+			'caption'     => $attachment->post_excerpt,
+			'description' => $attachment->post_content,
+			'href'        => get_permalink( $attachment->ID ),
+			'src'         => $attachment->guid,
+			'title'       => $attachment->post_title
+		);
+	}
+
+	/**
+	 * Getting the post_types which will be displayed in the sitemap
+	 *
+	 * @return array|string
+	 */
+	private function get_post_types() {
+
+		// Get supported post types
+		$post_types = array();
+		foreach ( get_post_types( array( 'public' => true ), 'objects' ) as $posttype ) {
+			if ( isset( $this->options['newssitemap_include_' . $posttype->name] ) && ( 'on' == $this->options['newssitemap_include_' . $posttype->name] ) ) {
+				$post_types[] = $posttype->name;
+			}
+		}
+
+		if ( count( $post_types ) > 0 ) {
+			$post_types = "'" . implode( "','", $post_types ) . "'";
+		} else {
+			$post_types = "'post'";
+		}
+
+		return $post_types;
+	}
+
+	/**
+	 * Getting all the items for the sitemap
+	 *
+	 * @param array $post_types
+	 *
+	 * @return mixed
+	 */
+	private function get_items( $post_types ) {
+		global $wpdb;
+
+		// Get posts for the last two days only, credit to Alex Moss for this code.
+		$items = $wpdb->get_results( "SELECT ID, post_content, post_name, post_author, post_parent, post_date_gmt, post_date, post_date_gmt, post_title, post_type
+									FROM $wpdb->posts
+									WHERE post_status='publish'
+									AND (DATEDIFF(CURDATE(), post_date_gmt)<=2)
+									AND post_type IN ($post_types)
+									ORDER BY post_date_gmt DESC
+									LIMIT 0, 1000" );
+
+		return $items;
+	}
+
+	/**
+	 * Getting the ketwords for given $item_id
+	 *
+	 * @param integer $item_id
+	 *
+	 * @return string
+	 */
+	private function get_item_keywords( $item_id ) {
+		$keywords = explode( ',', trim( WPSEO_Meta::get_value( 'newssitemap-keywords', $item_id ) ) );
+		$tags     = get_the_terms( $item_id, 'post_tag' );
+		if ( $tags ) {
+			foreach ( $tags as $tag ) {
+				$keywords[] = $tag->name;
+			}
+		}
+
+		// TODO: add suggested keywords to each post based on category, next to the entire site
+		if ( isset( $this->options['default_keywords'] ) && $this->options['default_keywords'] != '' ) {
+			$keywords = array_merge( $keywords, explode( ',', $this->options['default_keywords'] ) );
+		}
+
+		$keywords = strtolower( trim( implode( ', ', $keywords ), ', ' ) );
+
+		return $keywords;
+	}
+
+	/**
+	 * Getting the genre for given item
+	 *
+	 * @param integer $item_id
+	 *
+	 * @return string
+	 */
+	private function get_item_genre( $item_id ) {
+		$genre = WPSEO_Meta::get_value( 'newssitemap-genre', $item_id );
+		if ( is_array( $genre ) ) {
+			$genre = implode( ',', $genre );
+		}
+
+		if ( $genre == '' && isset( $this->options['default_genre'] ) && $this->options['default_genre'] != '' ) {
+			$genre = $this->options['default_genre'];
+		}
+		$genre = trim( preg_replace( '/^none,?/', '', $genre ) );
+
+		return $genre;
+	}
+
+	/**
+	 * Getting the image output for given $item
+	 *
+	 * @param object $item
+	 *
+	 * @return string
+	 */
+	private function get_item_image_output( $item ) {
+
+		$images = $this->get_item_images( $item );
+
+		if ( isset( $images ) && count( $images ) > 0 ) {
+			$output = '';
+			foreach ( $images as $src => $img ) {
+				$output .= $this->parse_item_image( $src, $img, $item );
+			}
+
+			return $output;
+		}
+	}
+
+	/**
+	 * Getting the images for the given $item
+	 *
+	 * @param object $item
+	 *
+	 * @return array
+	 */
+	private function get_item_images( $item ) {
+
+		static $home_url;
+
+		if ( $home_url == null ) {
+			$home_url = home_url();
+		}
+
+		$images = array();
+
+		if ( ( ! isset( $this->options['restrict_sitemap_featured_img'] ) || ! $this->options['restrict_sitemap_featured_img'] ) && preg_match_all( '/<img [^>]+>/', $item->post_content, $matches ) ) {
+			foreach ( $matches[0] as $img ) {
+				if ( preg_match( '/src=("|\')([^"|\']+)("|\')/', $img, $match ) ) {
+					$src = $match[2];
+					if ( strpos( $src, 'http' ) !== 0 ) {
+
+						if ( $src[0] != '/' ) {
+							continue;
+						}
+
+						$src = $home_url . $src;
+					}
+
+					if ( $src != esc_url( $src ) ) {
+						continue;
+					}
+
+					if ( isset( $url['images'][$src] ) ) {
+						continue;
+					}
+
+					$image = array();
+					if ( preg_match( '/title=("|\')([^"\']+)("|\')/', $img, $match ) ) {
+						$image['title'] = str_replace( array( '-', '_' ), ' ', $match[2] );
+					}
+
+					if ( preg_match( '/alt=("|\')([^"\']+)("|\')/', $img, $match ) ) {
+						$image['alt'] = str_replace( array( '-', '_' ), ' ', $match[2] );
+					}
+
+					$images[$src] = $image;
+				}
+			}
+		}
+
+		// Also check if the featured image value is set.
+		$post_thumbnail_id = get_post_thumbnail_id( $item->ID );
+
+		if ( '' != $post_thumbnail_id ) {
+
+			$attachment = $this->get_attachment( $post_thumbnail_id );
+
+			if ( count( $attachment ) > 0 ) {
+
+				$image = array();
+
+				if ( '' != $attachment['title'] ) {
+					$image['title'] = $attachment['title'];
+				}
+
+				if ( '' != $attachment['alt'] ) {
+					$image['alt'] = $attachment['alt'];
+				}
+
+				if ( '' != $attachment['src'] ) {
+					$images[$attachment['src']] = $image;
+				} elseif ( '' != $attachment['href'] ) {
+					$images[$attachment['href']] = $image;
+				}
+
+			}
+
+		}
+
+		return $images;
+	}
+
+	/**
+	 * Parse the XML for given image
+	 *
+	 * @param string $src
+	 * @param string $img
+	 * @param object $item
+	 *
+	 * @return string
+	 */
+	private function parse_item_image( $src, $img, $item ) {
+
+		$output = '';
+
+		/**
+		 * Filter: 'wpseo_xml_sitemap_img_src' - Allow changing of sitemap image src
+		 *
+		 * @api string $src The image source
+		 *
+		 * @param object $item The post item
+		 */
+		$src = apply_filters( 'wpseo_xml_sitemap_img_src', $src, $item );
+
+		$output .= "\t\t<image:image>\n";
+		$output .= "\t\t\t<image:loc>" . htmlspecialchars( $src ) . "</image:loc>\n";
+
+		if ( isset( $img['title'] ) ) {
+			$output .= "\t\t\t<image:title>" . htmlspecialchars( $img['title'] ) . "</image:title>\n";
+		}
+
+		if ( isset( $img['alt'] ) ) {
+			$output .= "\t\t\t<image:caption>" . htmlspecialchars( $img['alt'] ) . "</image:caption>\n";
+		}
+
+		$output .= "\t\t</image:image>\n";
+
+		return $output;
 	}
 
 }
